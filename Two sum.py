c889a0b363f53ca0deba9fc5caf8e1f62ce90a4a
Sisiyo Takele <149423407+sisiyotakele@users.noreply.github.com>
2026-02-09 23:36:41 +0300

    Create Two sum.py

---
+++ Two sum.py
@@ -0,0 +1,6 @@
+prevMap = {} 
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
