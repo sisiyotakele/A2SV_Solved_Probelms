ae2c47f6550329000eaa74579a9b40aafdf3ac14
Sisiyo Takele <149423407+sisiyotakele@users.noreply.github.com>
2026-02-19 18:02:56 +0300

    Create  Maximum Number of Coins You Can Get.py

---
+++ Maximum Number of Coins You Can Get.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        l = 0
+        r = len(piles) - 1
+        res = 0 
+        while l < r:
+            res += piles[r-1] 
+            l += 1
+            r -= 2
+        return res
+
+        
+     
+
+
+
+
