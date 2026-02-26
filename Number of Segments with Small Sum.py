4636392d752c93fe25f9694f24d26336d2406735
Sisiyo Takele <149423407+sisiyotakele@users.noreply.github.com>
2026-02-26 22:33:27 +0300

    Create  Number of Segments with Small Sum.py

---
+++ Number of Segments with Small Sum.py
@@ -0,0 +1,13 @@
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+left = 0 
+curr_sum = 0
+max_sum = 0 
+for right in range(n):
+    curr_sum += nums[right]
+ 
+    while curr_sum > s:
+        curr_sum -= nums[left]
+        left += 1
+    max_sum  += right -left +1
+print(max_sum)
