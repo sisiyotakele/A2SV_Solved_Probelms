fa422737429b3cf5eb73e62b003a8dbd7022a430
Sisiyo Takele <149423407+sisiyotakele@users.noreply.github.com>
2026-04-02 15:08:52 +0300

    Add solution for Word Break

--- leetcode/0139-word-break/0139-word-break.py
+++ leetcode/0139-word-break/0139-word-break.py
@@ -1,15 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         mapp = set(wordDict)
-        @cache
+        memo = {}
         def backtrack(ind):
             if ind >= len(s):
                 return True
+            if ind in memo:
+                return memo[ind]
             cur = ""
             for i in range(ind,len(s)):
                 cur += s[i]
                 if cur in mapp:
                     if backtrack(i + 1):
+                        memo[ind] = True
                         return True
+            memo[ind] = False
             return False
         return backtrack(0)
