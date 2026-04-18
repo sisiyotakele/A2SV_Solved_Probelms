8acdfe55043e704c3412bb3d58d55059675648d3
Sisiyo Takele <149423407+sisiyotakele@users.noreply.github.com>
2026-04-18 23:00:25 +0300

    Add solution for Sort Colors

---
+++ leetcode/0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l,m,r=0,0,len(nums)-1
+        while m <= r:
+            if nums[m]==0:
+                nums[l],nums[m]=nums[m], nums[l]
+                l+=1
+                m+=1
+            elif nums[m]==1:
+                m+=1
+
+            else:
+                nums[r],nums[m]  = nums[m], nums[r]
+                r-=1
+               
